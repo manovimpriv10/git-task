f9684b5317a7a7a02c8b363a4c9121c50503566d
Nikita Manov <nikitamanov49@gmail.com>
2024-10-05 17:50:43 +0300

    Add is leap year program

---
+++ DateTime.pas
@@ -0,0 +1,15 @@
+﻿begin
+  var year := ReadInteger('Введите год: ');
+Assert(year > 0);
+if year mod 4 = 0 then
+  if (year.Divs(100)) and (year.NotDivs(400)) then
+    year := 365
+  else
+    year := 366
+else
+  year := 365;
+if year = 365 then
+  Println('False')
+else
+  Println('True');
+end.
