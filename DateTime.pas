e0d485e14f1c00531d28ad084d62e3b2fb809ef8
Nikita Manov <nikitamanov49@gmail.com>
2024-10-05 18:03:42 +0300

    Add what day is clother to New Year

--- DateTime.pas
+++ DateTime.pas
@@ -12,4 +12,13 @@ if year = 365 then
   Println('False')
 else
   Println('True');
+var (date1, mounth1) := ReadInteger2('Введите день и месяц первой даты: ');
+var (date2, mounth2) := ReadInteger2('Введите день и месяц второй даты: ');
+if mounth1 > mounth2 then
+  Println(1)
+else
+if (mounth1 = mounth2) and (date1 > date2) then
+  Println(1)
+else
+  Println(2);
 end.
